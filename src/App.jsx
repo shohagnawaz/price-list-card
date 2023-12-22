d9e8d3deb9affa55b684f6313a89f1fcd4c9c932
shohagnawaz <snawazdigital@gmail.com>
2023-12-22 18:49:17 +0600

    price-card

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1 className="text-7xl text-purple-700 text-center">Md. Shah Nawaz</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const routes = [
+        {
+            id: 1,
+            name: 'Home',
+            path: '/'
+        },
+        {
+            id: 2,
+            name: 'About',
+            path: '/about'
+        },
+        {
+            id: 3,
+            name: 'Contact',
+            path: '/contact'
+        },
+        {
+            id: 4,
+            name: 'Products',
+            path: '/products'
+        },
+        {
+            id: 5,
+            name: 'Services',
+            path: '/services'
+        }
+    ]
+
+    return (
+        <nav className='bg-purple-400'>
+            <div onClick={() => setOpen(!open)} className='md:hidden'>
+                <span>
+                    {
+                        open === true ? 
+                            <XMarkIcon className="h-6 w-6 text-purple-900" />
+                            : <Bars3Icon className="h-6 w-6 text-purple-900" />
+                    }
+                </span>
+                
+                
+            </div>
+            <ul className={`md:flex md:static absolute duration-500 pl-4 pb-4 py-3 bg-purple-400 ${open ? 'top-6' : '-top-36'}`}>
+                {
+                    routes.map(route => <Link
+                        key={route.id}
+                        route={route}
+                    ></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
